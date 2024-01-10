8c1599462e1185a5017f677c4d58151ab81ad435
Anirban Basu <anirbanbasu@users.noreply.github.com>
2024-01-10 23:03:12 +0900

    fix: Tweaks based on various information from the Next.js documentation.

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,16 +1,19 @@
-import { createClient } from "next-sanity";
+import 'server-only'
+import { createClient, type ClientConfig } from "next-sanity";
 
 import imageUrlBuilder from '@sanity/image-url'
 import { Image } from "sanity";
 import { ProfileSummary } from "@/types/ProfileSummary";
 import { groq } from "next-sanity";
 
-const sanityClientConfig = {
+const sanityClientConfig: ClientConfig = {
     projectId: 'l7tokq15',
     dataset: 'production',
     apiVersion: '2023-08-31',
-    useCdn: true,
+    useCdn: true, // set to 'false' to bypass the edge cache
 }
+
+export const revalidateTime = 1800; // revalidate at most every half hour
 
 const sanityClient = createClient(sanityClientConfig);
 
@@ -35,6 +38,6 @@ export async function getProfileSummary(profileId: string): Promise<ProfileSumma
                 ...,
                 "imageData": imageData.asset -> {...}
             }
-        }[0]`, {cache: 'no-store'}
+        }[0]`, {next: {revalidate: revalidateTime}}
     );
 }
