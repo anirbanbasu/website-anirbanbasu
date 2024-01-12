165171fbd65b9e9b398900ed9daa648b02242566
Anirban Basu <anirbanbasu@users.noreply.github.com>
2024-01-12 22:39:01 +0900

    feat: new boxy logo for 'anirbanbasu'.

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <header className="py-6 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
         <div className="max-w-6xl mx-auto flex items-center justify-between">
           <Link href="/">
-            <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" priority src="/logo.svg" width={234} height={72} alt="logo" />
+            <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" priority src="/logo.svg" width={220} height={50} alt="logo" />
           </Link>
           <nav>
             <ul className="flex items-center gap-x-8">
