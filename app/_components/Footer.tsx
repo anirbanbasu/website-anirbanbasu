9d033f60c88d1693f8b31def7eac164bf22f0cce
Anirban Basu <anirbanbasu@users.noreply.github.com>
2024-01-13 23:40:45 +0900

    feat: Attempted responsive one-page site.

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,9 +4,9 @@ import { BiLogoGithub } from "react-icons/bi";
 
 export default function Footer() {
     return (
-      <footer className="border-t border-zinc-800 mt-4">
-        <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-6 text-zinc-400">
-          <small className="duration-200 font-mono justify-between items-center flex">
+      <footer>
+        <div className="w-full flex flex-wrap items-center justify-between gap-y-4 px-6 py-6 text-zinc-800">
+          <small className="duration-200 hover:text-white font-mono justify-between items-center flex">
             &copy;&nbsp;{new Date().getFullYear()}, Some Fancy-named Website Project. All rights reserved.
           </small>
   
--- app/page.tsx
+++ app/page.tsx
@@ -11,32 +11,34 @@ export default async function Home() {
   const profile:ProfileSummary = await getProfileSummary(profileId)
 
   return (
-    <main className="lg:max-w-7xl mx-auto max-w-3xl md:px-16 px-6">
-      <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
-      <Image
-            className="relative z-10 w-1/3 rounded-lg"
-            src={urlFor(profile.profileImage.imageData).quality(100).size(350,350).fit('min').url().toString()}
-            alt={profile.profileImage.altText}
-            width={profile.profileImage.imageData.metadata.dimensions.width}
-            height={profile.profileImage.imageData.metadata.dimensions.height}
-            priority={false}
-          />
-        <div key={profile._id} className="lg:max-w-2xl max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
-            {profile.name}
-          </h1>
-          <h2 className="text-xl font-extralight tracking-tight sm:text-2xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
-            {profile.tagLine}
-          </h2>
-          <div className="text-base text-justify text-zinc-400 leading-relaxed">
+    <main className="h-screen p-10 w-full flex items-center justify-center">
+      <section className="p-5 border-2 border-zinc-600 rounded-lg">
+        <div key={profile._id} className="flex items-center">
+          <Image
+                className="object-contain w-70 h-70 mr-8 rounded-lg"
+                src={urlFor(profile.profileImage.imageData).quality(100).size(350,350).fit('min').url().toString()}
+                alt={profile.profileImage.altText}
+                width={140}
+                height={140}
+                priority={false}
+              />
+          <div>
+            <h1 className="font-display mb-2 md:text-4xl text-2xl font-semibold dark:text-gray-200">
+              {profile.name}
+            </h1>
+            <h2 className="md:text-2xl font-extralight tracking-tight text-xl mb-6">
+              {profile.tagLine}
+            </h2>
+          </div>
+        </div>
+        <div className="text-base mt-4 mb-4 text-justify">
             <PortableText value={profile.summary} />
           </div>
-          <ul className="flex items-center gap-x-6 my-10">
-              {profile.keywords && profile.keywords.map((keyword, index) => (
-                  <li key={index} className="items-center flex justify-between text-sm text-pretty"><PiTagChevronDuotone />&nbsp;{keyword}</li>
-                ))}
-            </ul>
-        </div>
+        <ul className="border-t  border-zinc-800 pt-4 flex flex-wrap items-center gap-4">
+            {profile.keywords && profile.keywords.map((keyword, index) => (
+                <li key={index} className="items-center flex justify-between text-sm text-pretty"><PiTagChevronDuotone />&nbsp;{keyword}</li>
+              ))}
+        </ul>
       </section>
     </main>
   )
