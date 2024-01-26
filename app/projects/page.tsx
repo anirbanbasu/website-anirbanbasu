2684d1a06357006317436bcfe7cb6653617cbb54
Anirban Basu <anirbanbasu@users.noreply.github.com>
2024-01-26 23:21:31 +0900

    feat: Changed the project layout to use Mamba UI style component.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -22,58 +22,31 @@ export default async function Home() {
   return (
     <main className="flex items-center justify-center">
       <section className="mt-20 md:mt-24 max-w-7xl p-2">
-        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-                {projects && projects.map((project, index) => {
-                        if(index % 2 !== 0) {
-                            return (
-                                <li key={project._id}>
-                                    <div className="timeline-middle">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                    </div>
-                                    <div className="timeline-start md:text-end mb-10">
-                                        <time className="font-mono italic">{project.startDate} - {(project.endDate !== null? project.endDate : ongoing)}</time>
-                                        <div className="text-lg font-black">{project.name}</div>
-                                        <Image
-                                            src={urlFor(project.representativePicture.imageData).quality(100).size(Math.round(project.representativePicture.imageData.metadata.dimensions.width/4),Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)).fit('min').url().toString()}
-                                            alt={project.representativePicture.caption}
-                                            title={project.representativePicture.caption}
-                                            width={Math.round(project.representativePicture.imageData.metadata.dimensions.width/4)}
-                                            height={Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)}
-                                            priority={false}
-                                            />
-                                        <div className="text-justify"><PortableText value={project.description}/></div>
-                                    </div>
-                                    <hr/>
-                                </li>
-                            )
-                        }
-                        else {
-                            return (
-                                <li key={project._id} className='relative'>
-                                    <hr />
-                                    <div className="timeline-middle">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                    </div>
-                                    <div className="timeline-end mb-10">
-                                        <time className="font-mono italic">{project.startDate} - {(project.endDate !== null? project.endDate : ongoing)}</time>
-                                    <div className="text-lg font-black">{project.name}</div>
-                                    <Image
-                                            src={urlFor(project.representativePicture.imageData).quality(100).size(Math.round(project.representativePicture.imageData.metadata.dimensions.width/4),Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)).fit('min').url().toString()}
-                                            alt={project.representativePicture.caption}
-                                            title={project.representativePicture.caption}
-                                            width={Math.round(project.representativePicture.imageData.metadata.dimensions.width/4)}
-                                            height={Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)}
-                                            priority={false}
-                                            />
-                                    <div className="text-justify"><PortableText value={project.description}/></div>
-                                    </div>
-                                    <hr />
-                                </li>
-                            )
-                        }
-                    }
-                )}
-            </ul>
+       <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+            {projects && projects.map((project, index) => {
+                    return (
+                        <article key={project._id} className="flex flex-col">
+                            <a rel="noopener noreferrer" href={(project.webURL!==null? project.webURL : '#')} aria-label="Link to project URL">
+                            <Image
+                                    src={urlFor(project.representativePicture.imageData).quality(100).size(Math.round(project.representativePicture.imageData.metadata.dimensions.width/4),Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)).fit('min').url().toString()}
+                                    alt={project.representativePicture.caption}
+                                    title={project.representativePicture.caption}
+                                    width={Math.round(project.representativePicture.imageData.metadata.dimensions.width/4)}
+                                    height={Math.round(project.representativePicture.imageData.metadata.dimensions.height/4)}
+                                    className="object-cover w-full h-52"
+                                    priority={false}
+                                    />
+                            </a>
+                            <div className="flex flex-col flex-1 p-6">
+                                <a rel="noopener noreferrer" href="#" className="text-xs tracking-tight uppercase hover:underline">{project.startDate} - {(project.endDate !== null? project.endDate : ongoing)}</a>
+                                <h3 className="flex-1 py-2 text-lg font-semibold leading-tight">{project.name}</h3>
+                                <div className="text-justify line-clamp-5"><PortableText value={project.description}/></div>
+                            </div>
+                        </article>
+                    )
+                }
+            )}
+        </div>
       </section>
     </main>
   )
